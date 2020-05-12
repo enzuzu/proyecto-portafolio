ab0134e4abd6492a3504489223579ac1588cbe26
enzuzu <60944979+enzuzu@users.noreply.github.com>
2020-05-12 14:58:56 -0500

    12.mayo.2020

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/*
+const proyectoUno = document.getElementById("proyecto-uno");
+const imagenProyectoUno = document.querySelector(".imagen-proyecto");
+
+proyectoUno.addEventListener("click", displayProyectoImage);
+
+function displayProyectoImage() {
+  imagenProyectoUno.removeAttribute("hidden");
+  console.log(proyectoUno);
+}
+*/
+// TODO Ejemplo Mich: On Link Hover effect display proyecto image
